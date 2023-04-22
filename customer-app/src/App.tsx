416ff841c1544bbd6113dbfdad718ebd47f94bf5
PreetiRajguru <rajgurupreeti0@gmail.com>
2023-04-22 23:02:54 +0530

    refactor: added axios interceptor

--- customer-app/src/App.tsx
+++ customer-app/src/App.tsx
@@ -4,60 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Customers from './components/Customers';
 import Dashboard from './components/Dashboard';
 import EditCustomer from './components/EditCustomer';
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 import axios from 'axios';
-import React from 'react';
-
-export const AppContext = React.createContext<any>({});
+import { AppContext } from './config/https';
 
 function App() {
-
   useEffect(() => {
     axios.defaults.baseURL = 'https://localhost:7041';
   });
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    axios.interceptors.request.use(
-      function (config) {
-        console.log("Before Request..");
-        setLoading(true);
-        return config;
-      },
-      function (error) {
-        console.log("Error Before Request");
-        setLoading(false);
-        return Promise.reject(error);
-      }
-    );
-
-    axios.interceptors.response.use(
-      function (response) {
-        console.log("After Request..");
-        setLoading(false);
-        return response;
-      },
-      function (error) {
-        console.log("Error After Request");
-        setLoading(false);
-        return Promise.reject(error);
-      }
-    );
-  }, []);
+  const [loading] = useState(true);
 
 
   return (
     <AppContext.Provider value={{ loading }}>
     <div>
-
-    {loading && (
-          <div className="loader">
-            <div className="spinner"></div>
-            <div>Loading...</div>
-          </div>
-        )}
-
       <BrowserRouter>
         <Navigation />
         <Routes>
--- customer-app/src/components/Modal.tsx
+++ customer-app/src/components/Modal.tsx
@@ -3,16 +3,15 @@ import { Customer } from "./Customers";
 
 const Modal = ({customer, onClose}: {customer: Customer, onClose: () => void}) => {
     return (
-
         <Dialog open onClose={onClose}>
-            <DialogTitle>Customer data</DialogTitle>
+            <DialogTitle>Customer Data</DialogTitle>
             <DialogContent>
-                Name : {customer.firstName}&nbsp;
+                <b>Name : </b>{customer.firstName}&nbsp;
                 {customer.lastName}<br></br>
-                DOB : {customer.dateOfBirth}<br></br>
-                Phone Number : {customer.phoneNumber}<br></br>
-                Email : {customer.email}<br></br>
-                Address : {customer.address}<br></br>
+                <b>DOB : </b> {customer.dateOfBirth}<br></br>
+                <b>Phone Number : </b> {customer.phoneNumber}<br></br>
+                <b>Email : </b>{customer.email}<br></br>
+                <b>Address : </b>{customer.address}<br></br>
             </DialogContent>
         </Dialog>
     )
